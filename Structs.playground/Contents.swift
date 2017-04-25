317bf7fc15a14ffb525723da42628e839c7945fa
Farhan Syed <farhansyed11.3@gmail.com>
2017-04-25 13:48:08 -0700

    first commit

---
+++ Structs.playground/Contents.swift
@@ -0,0 +1,64 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+// Basic syntax of a struct
+
+struct Name {
+    // properties here
+}
+
+// Let's use an example where you have a User struct, it's properties are username, email, and name.
+
+struct User {
+    var username: String
+    var email: String
+    var name: String
+}
+
+// We can now create an instance of a struct. 
+
+// If you notice the compiler will provide us with parameters to fill of each property.
+
+var user = User(username: "farhansyed", email: "farhansyed123@gmail.com", name: "Farhan Syed")
+
+// To read a propery you simply 
+
+print(user.username)
+
+// It's important to know the difference between value & reference types
+
+// Structs are value type, meaning it's value is copied when assigned or passed.
+
+// Example:
+
+var user2 = User(username: "farhan", email: "someEmail@gmail.com", name: "Farhan")
+
+var user3 = user2
+
+user2.username = "FarhanSyed"
+
+print("user2 username is unchanged: \(user2.username)")
+print("user3 username: \(user3.username)")
+
+// Notice when printed the original, user2 is unchanged as a value type.
+
+
+// You can even add functions inside structs
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    
+    func printName() {
+        print("\(self.firstName) \(self.lastName)")
+    }
+}
+
+var person = Person(firstName: "Farhan", lastName: "Syed")
+
+// Let's call printName()
+
+person.printName()
+
+
